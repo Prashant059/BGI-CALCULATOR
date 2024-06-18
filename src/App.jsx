4dd77593365547504f8072265ef4e1a0162945ff
Prashant Randive <prashant.randhive05@gmail.com>
2024-06-18 15:41:17 +0530

    files Uploads

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BmiCalculator from './components/BmiCalculator'
+import './App.css'
+
+function App() {
+
+  return (
+    
+      <BmiCalculator />
+    
+  )
+}
+
+export default App
